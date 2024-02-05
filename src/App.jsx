6c6f1ea07682331b839bb29a2e92529378e100f9
pratyushcode <pratyush.sinha.ug21@nsut.ac.in>
2024-02-06 00:40:32 +0530

    made compatible  for dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 
 // import Login from './components/registration/Login';
 // import Signup from './components/registration/Signup';
@@ -48,6 +48,7 @@ export const ProtectedRoute = ({ children }) => {
     return <Navigate to={"/login"} />;
   }
 };
+
 
 // admin
 // const ProtectedRouteForAdmin = ({ children }) => {
