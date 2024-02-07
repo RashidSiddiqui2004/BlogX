d4fa86a73e3ff894429c0bb44818e361fd7a19a7
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2024-02-07 18:27:53 +0530

    Initial Category Section

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import TrendingBlogs from "./trending/Trending";
  
 import Navbar from "./navbar/Navbar";
-import FeaturedSection from './featured/FeaturedSection'
+import FeaturedSection from './featured/FeaturedSection';
+import CategorySection from './categories/Categories';
  
  
  
@@ -13,9 +14,13 @@ const Homepage = () => {
       
       <div className="TrendingBlogs  overflow-hidden w-[90%] m-auto">
         <FeaturedSection />
- 
+      </div>
 
-      <div className="TrendingBlogs  overflow-hidden w-full">
+      <div className="CategorySection  overflow-hidden w-[90%] m-auto">
+        <CategorySection />
+      </div>
+      
+      <div className="TrendingBlogs  overflow-hidden w-[90%] m-auto">
  
         <TrendingBlogs />
       </div>
