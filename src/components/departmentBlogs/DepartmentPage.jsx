7a619dbc61cec73f545daae0de4065a9d55d70dc
lucifer19807 <piyushsinha19807@gmail.com>
2024-02-10 12:50:16 +0530

    made some ui cahnge into categories section

--- src/components/departmentBlogs/DepartmentPage.jsx
+++ src/components/departmentBlogs/DepartmentPage.jsx
@@ -1,17 +1,17 @@
-
-import React from 'react'
-import Navbar from '../homepage/navbar/Navbar'
-import Footer from '../homepage/footer/Footer'
-import CategorySection from '../homepage/categories/Categories'
+import React from 'react';
+import Navbar from '../homepage/navbar/Navbar';
+import Footer from '../homepage/footer/Footer';
+import CategorySection from '../homepage/categories/Categories';
 
 const DepartmentPage = () => {
   return (
-    <div>
-        <Navbar/>
-        <CategorySection/>
-        <Footer/>
+    <div className='flex flex-col min-h-screen'>
+      <Navbar />
+      <CategorySection />
+      <div className='flex-grow'></div> 
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default DepartmentPage
+export default DepartmentPage;
