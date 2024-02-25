566802610a37185c63beefca0c071859eb1834e7
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2024-02-25 12:39:50 +0530

    featured section design changes with responsiveness

--- src/components/homepage/featured/FeaturedSection.jsx
+++ src/components/homepage/featured/FeaturedSection.jsx
@@ -1,7 +1,7 @@
-
 import React, { useContext, useEffect, useState } from "react";
 import myContext from "../../../context/data/myContext";
 import FeaturedBlog from "./FeaturedBlog";
+import "./FeaturedSection.css"; 
 
 function FeaturedSection() {
   const context = useContext(myContext);
@@ -13,22 +13,20 @@ function FeaturedSection() {
   useEffect(() => {
     const fetchFeaturedBlogs = async () => {
       await getFeaturedBlogs().then((featur_blogs) => {
-        setFeaturedBlogs(featur_blogs)
+        setFeaturedBlogs(featur_blogs);
       });
-    }
+    };
     fetchFeaturedBlogs();
   }, []);
-
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % numFeaturedBlogs);
-    }, 8000);
+    }, 7000);
     return () => clearInterval(intervalId);
   }, [numFeaturedBlogs]);
-
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex === numFeaturedBlogs - 1 ? 0 : prevIndex + 1));
@@ -41,18 +39,27 @@ function FeaturedSection() {
   return (
     <div className=" mt-4 md:mt-[2%]">
       <div className="md:w-[6%] md:mt-10">
-        <button className={`prev mr-3  ${mode === "dark"
-          ? "bg- rounded-lg text-white"
-          : "bg-neutral-80 text-zinc-800"
-          }`} onClick={handlePrev}>←</button>
-        <button className={`next mr-3  ${mode === "dark"
-          ? "bg- rounded-lg text-white"
-          : "bg-neutral-80 text-zinc-800"
-          }`} onClick={handleNext}>→</button>
+        <button
+          className={`prev mr-3 ${
+            mode === "dark" ? "bg- rounded-lg text-white" : "bg-neutral-80 text-zinc-800"
+          }`}
+          onClick={handlePrev}
+        >
+          ←
+        </button>
+        <button
+          className={`next mr-3 ${
+            mode === "dark" ? "bg- rounded-lg text-white" : "bg-neutral-80 text-zinc-800"
+          }`}
+          onClick={handleNext}
+        >
+          →
+        </button>
       </div>
-          
-        
-      <FeaturedBlog mode={mode} featuredBlogs ={featuredBlogs} currentIndex={currentIndex}/>
+
+      <div className="featured-blog-container md:mt-0 mt-[-10%]">
+        <FeaturedBlog mode={mode} featuredBlogs={featuredBlogs} currentIndex={currentIndex} />
+      </div>
 
       <div className="border-b border-gray-300 mt-0 md:mt-24"></div>
     </div>
