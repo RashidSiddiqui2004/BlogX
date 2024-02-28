a38cab4b24d98f632c2fbc8088ebe3368487cfaf
oi-taa <ishita21953@gmail.com>
2024-02-28 20:27:44 +0530

    Changed font size,width,alignment and weight

--- src/components/homepage/categories/Categories.jsx
+++ src/components/homepage/categories/Categories.jsx
@@ -26,16 +26,14 @@ function CategorySection() {
     },
     {
       id: 3,
-      title: "Machine Learning",
+      title: "AI/ML",
       image: MLDept,
       url: 'machine-learning'
     },
   ];
 
 
-
   const [currentIndex, setCurrentIndex] = useState(0);
-
 
   return (
     <div>
@@ -51,7 +49,6 @@ function CategorySection() {
           }`} >→</button>
       </div> */}
 
-
       <div className="flex flex-col justify-between md:flex-row md:py-12 md:h-[500px]">
 
         <div className="gap-5 mt-9  md:mt-[10%] md:w-[30%] max-md:flex-wrap">
@@ -63,13 +60,13 @@ function CategorySection() {
             style={{ display: "flex", flexDirection: "column", justifyContent: "flex-end" }}
           >
             <div className="flex flex-row gap-x-5">
-              <div className="text-sm text-sky-500 uppercase max-md:max-w-full">
+              <div className="text-start font-black text-sm uppercase max-md:max-w-full mb-2" style={{ color: '#0096FF' }}>
                 Categories
               </div>
             </div>
 
-            <div className="mt-5 text-3xl text-start space-around tracking-tight leading-12 max-md:max-w-full max-md:leading-10 md:mb-0 mb-10 md:text-5xl">
-              Dive into <br className="hidden md:block" /> Tech <span className="text-blue-400 text-normal">&#10216; / &#10217;</span>
+            <div className="text-start font-bold text-3xl text-start space-around tracking-tight leading-12 max-md:max-w-full max-md:leading-10 md:mb-0 mb-10 md:text-5xl mb-5">
+              Dive into <br className="hidden md:block" /> Tech <span style={{ color: '#0096FF' }} >&#10216; / &#10217;</span>
             </div>
 
             {/* <div
@@ -88,7 +85,7 @@ function CategorySection() {
         </div>
 
         <div className="md:w-[65%] w-[90%] mt-16 md:mt-2 mx-auto md:ml-auto md:mr-5 flex justify-center items-center transition-transform duration-500 ease-in-out md:mb-[-3.5%]">
-          <div className={`grid grid-cols-2 md:grid-cols-3 gap-5 justify-between md:h-[80%] h-[140%] ${mode === "dark" ? "bg- rounded-lg text-white" : "bg-neutral-80 text-gray-500"}`}>
+          <div className={`grid grid-cols-2 md:grid-cols-3 gap-x-5 md:gap-x-30 gap-y-8 md:gap-y-10 justify-between md:h-[80%] h-[140%] ${mode === "dark" ? "bg- rounded-lg text-white" : "bg-neutral-80 text-gray-500"}`}>
             {departmentData.map((dept) => (
               <Link to={`/department/${dept.url}`} key={dept.id}>
                 <Card
@@ -102,10 +99,12 @@ function CategorySection() {
 
       </div>
 
-
       <div className="border-b border-gray-300 md:mt-24 mt-24"></div>
     </div>
   );
 }
 
 export default CategorySection;
+
+
+
